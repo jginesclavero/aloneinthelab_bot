799937233fec77491b1b80e5c543bc289000c682
jgines <jonathangines@hotmail.com>
2017-10-30 20:03:57 +0100

    get request

--- bot.py
+++ bot.py
@@ -5,18 +5,28 @@ import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters, CallbackQueryHandler
-
+import requests
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+sched = Scheduler()
+sched.start()
+
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Hola! acabo de iniciarme, como estas?")
 
 def get_price(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Cuando este completo, aqui te devolvere el precio")
+
+def update_price():
+    print "Every 10 seconds"
+    r = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR")
+    print r.content
+
+
 
 def main():
 
@@ -31,6 +41,7 @@ def main():
 
     dispatcher.add_handler(price_handler)
     dispatcher.add_handler(start_handler)
+    sched.add_interval_job(update_price, seconds = 5)
 
     updater.start_polling()
 
