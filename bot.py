13b095499c0511124ed8184f985d8338dc3dc5ac
jgines <jonathangines@hotmail.com>
2017-10-31 17:47:01 +0100

    limits enable

--- bot.py
+++ bot.py
@@ -7,29 +7,48 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters, CallbackQueryHandler
 import requests
 import time, threading
+import re
+import json
 
 r = 0
+limit = 1.0
+bot_global = 0
+update_global = 0
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-#Variable global con el precio
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Hola! acabo de iniciarme, como estas?")
 
 def get_price(bot, update):
-    global r = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR")
-    #bot.send_message(chat_id=update.message.chat_id, text="Cuando este completo, aqui te devolvere el precio")
-    #print r.content
+    global r
+    global bot_global
+    global update_global
+    bot_global = bot
+    update_global = update
+    #r = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR")
+    #logger.info('Get_price')
     bot.send_message(chat_id=update.message.chat_id, text=r.content)
 
 
 def update_price():
-    global r = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR")
+    global r
+    global limit
+    global bot_global
+    global update_global
+    r = requests.get("https://min-api.cryptocompare.com/data/price?fsym=ETN&tsyms=USD,EUR")
     threading.Timer(30, update_price).start()
-    
+    data = json.loads(r.content)
+    #print (data["USD"])
+    if float(data["USD"]) >= limit:
+        bot_global.send_message(chat_id=update_global.message.chat_id, text="ETN tiene un valor de 1$, ya teneis para un viaje a Maldivas")
+
+def get_help(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="AloneInTheLab Bot, Mision ETN. Monitorizacion de precio y aviso cuando se alcance el valor de 1$. Comandos: /getPrice (Obtener precio actual del ETN)")
+
 
 def main():
 
@@ -41,10 +60,15 @@ def main():
 
     start_handler = CommandHandler("start", start)
     price_handler = CommandHandler("getPrice", get_price)
+    help_handler = CommandHandler("help", get_help)
+
+
     update_price()
 
     dispatcher.add_handler(price_handler)
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
+
 
     updater.start_polling()
     updater.idle()
